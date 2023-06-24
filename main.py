75d4811ea0ebb6a527325a7d4ad1c4c916fd6be5
Shizuka <tkpp26@gmail.com>
2023-06-24 16:55:18 -0700

    Edit main.py and add flowchart

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def main():
     celcius = int(input("Enter the temperature in celsius: "))
     fahrenheit = (9/5)*celcius+32
 
-    print(f"Temperature in Farenheit: {fahrenheit:.2f}")
-    
+    print(f"Temperature in Fahrenheit: {fahrenheit:.2f}")
 
     """
     ########################################
